53942f6f13f7544e0976a25fa2ae6461fbe6ade7
wathika-eng <wathika02@gmail.com>
2024-05-30 10:53:13 +0300

    fixed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    """
+    Print the Fibonacci sequence up to the specified length.
+    """
+    if length <= 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        num = [0, 1]
+        for i in range(2, length):
+            num.append(num[i - 1] + num[i - 2])
+        print(num)
+
+
+print_fibonacci(9)
